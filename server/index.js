6fd2545102b24e3524f42bec52fd82b90455bbdb
Jaheen Reza <rezajaheen@gmail.com>
2025-04-18 20:12:28 -0400

    added json files for activities and friends

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 const express = require('express')
+const activityController = require('./controller/activity')
 const PORT = 8000
 
 const app = express();
 
-app.get('/', (req, res) => {
+app
+.get('/', (req, res) => {
   res.send('Hello New Paltz, NY!!!')
 })
-
+.use('/activities', activityController)
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}/`)
